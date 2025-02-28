022820e407a30d550d702cfe3999228beecd9fa6
SereywatPro <sereywat999@gmail.com>
2025-02-28 18:32:14 +0700

    Third commit

--- components/Script.js
+++ components/Script.js
@@ -1,37 +1,32 @@
-"use strict"
+"use strict";
 
 const cardEl = document.querySelectorAll(".card");
-const myInput = document.querySelector('.myInput');
+const myInput = document.querySelector(".myInput");
 const textCard = document.querySelectorAll("p");
- const conNotFound = document.querySelector(".container-404");
+const conNotFound = document.querySelector(".container-404");
 
-myInput.addEventListener("change", (e) => {
-    const SearchCovert = e.target.value;
-    const covert = SearchCovert.toLowerCase().trim();
-    let found = false;
-    console.log(covert)
-
-    for (let items of textCard) {
-        console.log(items)
-        const texCards = items.textContent.toLowerCase().trim();
-        console.log(texCards)
-        if (texCards.indexOf(covert) !== -1) {
-            items.style.display = "block"; 
-            found = true;
-        } else {
-            items.style.display = "none"
-            
-        }
-
-    }
-    if (found) {
-        conNotFound.style.display = "none";
-        found = false;
-        
+myInput.addEventListener("input", (e) => {
+  const SearchCovert = e.target.value;
+  const covert = SearchCovert.toLowerCase().trim();
+  let found = false;
+  console.log(covert);
+  if (SearchCovert === "") {
+    textCard.forEach((text) => (text.style.display = "none"));
+    return;
+  }
+  for (let text of textCard) {
+    const texCards = text.textContent.toLowerCase().trim();
+    if (texCards.includes(covert)) {
+      text.style.display = "block";
+      found = true;
+      conNotFound.style.display = "none";
     } else {
-        conNotFound.style.display = "block"
+      text.style.display = "none";
     }
-    
-    
-
+  }
+  if (!found) {
+    conNotFound.style.display = "block";
+  } else {
+    conNotFound.style.display = "none";
+  }
 });
